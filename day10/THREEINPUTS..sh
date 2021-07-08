68678e9c92abda9052a6d216ccac2a9baef73cd6
ASHIQ40788 <ashiqshaiksvali8@gmail.com>
2021-07-08 19:45:22 +0530

    Added:THREEINPUTS

--- day10/THREEINPUTS..sh
+++ day10/THREEINPUTS..sh
@@ -1,10 +1,16 @@
-
 #!/bin/bash -x
-
-echo "enter three inputs :"
-read a
-read b
-read c
-
-val=$(($a + $b * $c))
-echo vaule1=$val
+# To declare static Array 
+arr=(26 34 6 3)
+i=0
+  
+# Loop upto size of array
+# starting from index, i=0
+while [ $i -lt ${#arr[@]} ]
+do
+    # To print index, ith
+    # element
+    echo ${arr[$i]}
+      
+    # Increment the i = i + 1
+    i=`expr $i--`
+done
--- day10/THREEINPUTS.sh
+++ day10/THREEINPUTS.sh
@@ -1,22 +1,6 @@
-
 #!/bin/bash -x
 
-echo "enter three inputs :"
-read a
-read b
-read c
-
-val=$(($a + $b * $c))
-echo value1=$val
-
-val=$(($a * $b + $c))
-echo value2=$val
-
-
-val=$(($c + $a / $b))
-echo value3=$val
-
-val=$(($a / $b + $c))
-echo value4=$val
-
-
+for i in $( echo "10 -29 33 67 -6 7 -10" | tr ' ' '\n' | sort -nr )
+do
+  echo $i
+done
